a99c685832105997292ec3259adb7e2dea24c6e0
MartiniGlauber <gmartinisantos@gmail.com>
2024-04-25 21:14:41 -0300

    finalizando responsividade e menu hamburguer para nav

--- ProjWebAlternativa/src/js/if.especial.js
+++
@@ -1,8 +0,0 @@
-// Adiciona um evento de clique ao ícone apenas quando a largura da tela for menor que 768px
-if (window.matchMedia("(max-width: 500px)").matches) {
-    // Adiciona um evento de clique ao ícone
-    document.getElementById("imagemCardRequisitos").addEventListener("click", function() {
-        // Alterna a visibilidade do parágrafo ao clicar no ícone
-        document.getElementById("texto").classList.toggle("texto-escondido");
-    });
-}
